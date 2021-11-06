953aa7f9e0471e89d588aaad2d7bdbc6f89e96f7
Jensen Hernández <Jess232016@outlook.com>
2021-11-05 22:28:57 -0600

    agregado serviceWorker

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorker from './serviceWorker.js';
 
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
